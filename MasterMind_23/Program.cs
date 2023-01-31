86fa45183388e20d5d6226ae76637e5fbb28100f
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-01-31 15:16:11 +0100

    lost preview

--- MasterMind_23/Program.cs
+++ MasterMind_23/Program.cs
@@ -14,7 +14,7 @@ namespace MasterMind_23
             Game game = new Game();
             game.CreateCode();
             //how many guesses loop
-            while(game.Guesses.Count < 12)
+            while (game.Guesses.Count < 12)
             {
                 Console.WriteLine("Make your choice");
                 //which input are allowed
@@ -36,19 +36,20 @@ namespace MasterMind_23
                     Console.WriteLine();
                 }
                 Console.WriteLine();
-            }
-            if(game.Guess.Count >= 12)
-            {
-                Console.WriteLine("You have lost");
+
+                if (game.Guesses.Count >= 12)
+                {
+                    Console.WriteLine("You have lost");
+                }
             }
         }
-       //display right colors
+        //display right colors
         public static void WritePin(Color color)
         {
-            switch(color)
+            switch (color)
             {
                 case Color.Blue:
-                    Console.ForegroundColor = ConsoleColor.Blue; 
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     break;
                 case Color.Red:
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -72,126 +73,132 @@ namespace MasterMind_23
                     Console.ForegroundColor = ConsoleColor.Green;
                     break;
             }
-            Console.OutputEncoding = Encoding.Unicode;
+
             Console.Write("\x25A0 ");
             Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
-}
-public class Game
-{
-    public List<Pin> Code { get; set; }
-    public Dictionary<int, List<Pin>> Guesses { get; set; }
 
-    public Game() 
-    { 
-        Code = new List<Pin>();
-        Guesses = new Dictionary<int, List<Pin>>();
-    }
-
-    public void CheckCode(List<Pin> guessCode)
+    public class Game
     {
-        List<ResultPin>  resultPins = new List<ResultPin>();
-        foreach(var pin in guessCode)
+        public List<Pin> Code { get; set; }
+        public Dictionary<int, List<Pin>> Guesses { get; set; }
+
+        public Game()
         {
-            //correct answer condition
-            if (Code.FirstOrDefault(x => x.Color == pin.Color && x.Position == pin.Position) != null)
+            Code = new List<Pin>();
+            Guesses = new Dictionary<int, List<Pin>>();
+        }
+
+        public void CheckCode(List<Pin> guessCode)
+        {
+            List<ResultPin> resultPins = new List<ResultPin>();
+            foreach (var pin in guessCode)
             {
-                resultPins.Add(new ResultPin(ResultType.Correct));
-            }
-            else if(Code.Any(x => x.Color == pin.Color))
-            {
-                resultPins.Add(new ResultPin(ResultType.Wrong));
+                //correct answer condition
+                if (Code.FirstOrDefault(x => x.Color == pin.Color && x.Position == pin.Position) != null)
+                {
+                    resultPins.Add(new ResultPin(ResultType.Correct));
+                }
+                else if (Code.Any(x => x.Color == pin.Color))
+                {
+                    resultPins.Add(new ResultPin(ResultType.Wrong));
+                }
+                else
+                {
+                    resultPins.Add(new ResultPin(ResultType.Malplaced));
+                }
             }
         }
-    }
-    // Random color generator. Get the 4 colors to guess on. 
-    public void CreateCode()
-    {
-        Random random = new Random();
-
-        for(int i = 0; i < 4; i++)
+        // Random color generator. Get the 4 colors to guess on. 
+        public void CreateCode()
         {
-            Color randomColor = (Color)random.Next(1, 8);
-            Code.Add(new Pin(i, randomColor));
-        }
-    }
-    public void MakeGuess(string guess)
-    {
-        List<string> inputs = guess.Split('.').ToList();
-        List<Pin> guessCode = new List<Pin>();
+            Random random = new Random();
 
-        foreach(string guessColor in inputs)
-        {
-            switch(guessColor)
+            for (int i = 0; i < 4; i++)
             {
-                case "B":
-                    guessCode.Add(new Pin(guessCode.Count, Color.Blue));
-                    break;
-                case "C":
-                    guessCode.Add(new Pin(guessCode.Count, Color.Cyan));
-                    break;
-                case "W":
-                    guessCode.Add(new Pin(guessCode.Count, Color.White));
-                    break;
-                case "Y":
-                    guessCode.Add(new Pin(guessCode.Count, Color.Yellow));
-                    break;
-                case "G":
-                    guessCode.Add(new Pin(guessCode.Count, Color.Green));
-                    break;
-                case "R":
-                    guessCode.Add(new Pin(guessCode.Count, Color.Red));
-                    break;
-                case "M":
-                    guessCode.Add(new Pin(guessCode.Count, Color.Magenta));
-                    break;
-                case "O":
-                    guessCode.Add(new Pin(guessCode.Count, Color.Orange));
-                    break;
+                Color randomColor = (Color)random.Next(1, 8);
+                Code.Add(new Pin(i, randomColor));
             }
-            
         }
-        Guesses.Add(Guesses.Count, guessCode);
+        public void MakeGuess(string guess)
+        {
+            List<string> inputs = guess.Split('.').ToList();
+            List<Pin> guessCode = new List<Pin>();
+
+            foreach (string guessColor in inputs)
+            {
+                switch (guessColor)
+                {
+                    case "B":
+                        guessCode.Add(new Pin(guessCode.Count, Color.Blue));
+                        break;
+                    case "C":
+                        guessCode.Add(new Pin(guessCode.Count, Color.Cyan));
+                        break;
+                    case "W":
+                        guessCode.Add(new Pin(guessCode.Count, Color.White));
+                        break;
+                    case "Y":
+                        guessCode.Add(new Pin(guessCode.Count, Color.Yellow));
+                        break;
+                    case "G":
+                        guessCode.Add(new Pin(guessCode.Count, Color.Green));
+                        break;
+                    case "R":
+                        guessCode.Add(new Pin(guessCode.Count, Color.Red));
+                        break;
+                    case "M":
+                        guessCode.Add(new Pin(guessCode.Count, Color.Magenta));
+                        break;
+                    case "O":
+                        guessCode.Add(new Pin(guessCode.Count, Color.Orange));
+                        break;
+                }
+
+            }
+            Guesses.Add(Guesses.Count, guessCode);
+            CheckCode(guessCode);
+        }
     }
-}
 
-public class ResultPin
-{
-    public ResultType Type { get; set; }
-
-    public ResultPin(ResultType type)
+    public class ResultPin
     {
-        Type = type;
+        public ResultType Type { get; set; }
+
+        public ResultPin(ResultType type)
+        {
+            Type = type;
+        }
     }
-}
 
-public enum ResultType
-{
-    Correct,
-    Wrong,
-    Malplaced
-}
-public class Pin
-{
-    public int Position { get; set; }
-    public Color Color { get; set; }
-
-    public Pin(int position, Color color)
+    public enum ResultType
     {
-        Position = position;
-        Color = color;
+        Correct,
+        Wrong,
+        Malplaced
     }
-}
-// The colors in the game
-public enum Color
-{
-    White = 1,
-    Cyan = 2,
-    Blue = 3,
-    Yellow = 4,
-    Green = 5,
-    Red = 6,
-    Magenta = 7,
-    Orange = 8
+    public class Pin
+    {
+        public int Position { get; set; }
+        public Color Color { get; set; }
+
+        public Pin(int position, Color color)
+        {
+            Position = position;
+            Color = color;
+        }
+    }
+    // The colors in the game
+    public enum Color
+    {
+        White = 1,
+        Cyan = 2,
+        Blue = 3,
+        Yellow = 4,
+        Green = 5,
+        Red = 6,
+        Magenta = 7,
+        Orange = 8
+    }
 }
