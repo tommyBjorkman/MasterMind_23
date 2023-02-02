3bf8d8f248a95ed3d22cc0ba386a78b12e2d2488
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-02-02 15:51:17 +0100

    last update, final commit

--- MasterMind_23/Program.cs
+++ MasterMind_23/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace MasterMind_23
 {
@@ -34,7 +32,7 @@ namespace MasterMind_23
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.Write("■ ");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("Beat the computer!");
+            Console.WriteLine("Now win the game!");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Choose between White, Cyan, Blue, Yellow, Green, Red, Silver or Magenta. ");
             Console.ForegroundColor = ConsoleColor.DarkYellow;
